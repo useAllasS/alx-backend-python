fa4f6d084a5b3ab21926f5ab6f76bae2a56cde7b
Your Name <you@example.com>
2024-07-05 11:44:58 +0000

    EE

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,6 +6,5 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    '''takes list of float and reture summed value 
-    '''
+    '''takes list of float and reture summed value'''
     return float(sum(input_list))
